09b860df606cef551b8a087bda4249e58be2efa3
Fransjunior <109284110+Fransjunior@users.noreply.github.com>
2023-05-18 10:31:08 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>J | k | E| Development</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="css/cabecera.css" type="text/css">
+</head>
+
+<body>
+
+</body>
+<?php include 'cabecera.html'; ?>
+
+<div>
+    <section class="vid has-background-light">
+        <div class="container has-text-centered">
+            <div class="embed-responsive embed-responsive-16by9">
+                <video class="vide embed-responsive-item" autoplay muted loop>
+                    <source src="vid/1.mp4" type="video/mp4">
+                    Tu navegador no soporta el elemento video.
+                </video>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <h2 class="title is-2">Nuestra historia</h2>
+                    <p>
+                        <li>Ubicado en la región de Apurímac en Perú, un grupo de apasionados por la tecnología que decidieron unirse y fundar una empresa de desarrollo web llamada "Frontend and Backend Developer". Corría el año 2023 y el equipo de emprendedores estaba decidido a llevar el poder de la tecnología a las empresas locales y ayudarlas a prosperar en el mundo digital.</li>
+                        <li>Los fundadores de "Frontend and Backend Developer" compartían una visión común: querían ser pioneros en la transformación digital de Andahuaylas y sus alrededores. Creían firmemente en el potencial del desarrollo frontend y backend para impulsar el crecimiento empresarial y estaban decididos a brindar soluciones tecnológicas de alta calidad a sus clientes.</li>
+                        <li>Con entusiasmo y determinación, el equipo comenzó a establecer su presencia en la comunidad empresarial local. Organizaron talleres y charlas gratuitas para educar a los empresarios sobre los beneficios de tener una sólida presencia en línea y cómo el desarrollo web podía potenciar sus negocios. Pronto, comenzaron a ser reconocidos como los expertos en desarrollo web en Andahuaylas.</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <h2 class="title is-2">Nuestra Ubicacion</h2>
+                </div> <br>
+                <div class="has-text-centered">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.9446467834537!2d-73.38039942531442!3d-13.661129774055583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x916d2bd13a696323%3A0x617a8636d0cb0af0!2sAv.%20Malinas%2C%20Andahuaylas%2003701!5e0!3m2!1ses-419!2spe!4v1684390109425!5m2!1ses-419!2spe" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+</html>
